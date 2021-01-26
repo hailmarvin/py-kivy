1a1fd96709de7b7c257a0fae05e52419b614ea8e
hailmarvin <mmwanzi76@gmail.com>
2021-01-26 23:31:26 +0300

    button and events

--- kivyapp.py
+++ kivyapp.py
@@ -3,28 +3,58 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+import os
 kivy.require("2.0.0")
 
 class ConnectPage(GridLayout):
+    # runs on initialization
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        if os.path.isfile("prev_details.txt"):
+            with open("prev_details.txt", "r") as f:
+                d = f.read().split(",")
+                prev_ip = d[0]
+                prev_port = d[1]
+                prev_username = d[2]
+        else:
+            prev_ip = ""
+            prev_port = ""
+            prev_username = ""
+
         self.cols = 2
         self.add_widget(Label(text='IP:'))
-        self.ip = TextInput(multiline=False)
+        self.ip = TextInput(text=prev_ip,multiline=False)
         self.add_widget(self.ip)
 
         self.add_widget(Label(text='Port:'))
-        self.port = TextInput(multiline=False)
+        self.port = TextInput(text=prev_port, multiline=False)
         self.add_widget(self.port)
 
         self.add_widget(Label(text='Username:'))
-        self.username = TextInput(multiline=False)
+        self.username = TextInput(text=prev_username, multiline=False)
         self.add_widget(self.username)
+
+        # Setting up button
+        self.join = Button(text="Join")
+        self.join.bind(on_press=self.join_button)
+        self.add_widget(Label())
+        self.add_widget(self.join)
+
+    def join_button(self, instance):
+        ip = self.ip.text
+        port = self.port.text
+        username = self.username.text
+
+        with open("prev_details.txt", "w") as f:
+            f.write(f"{ip},{port},{username}")
+        print(f"Joining {ip}:{port} as {username}")
+
 
 class EpicApp(App):
     def build(self):
-        return ConnectPage()  
+        return ConnectPage()
 
 if __name__ == '__main__':
-    EpicApp().run()              
+    EpicApp().run()
